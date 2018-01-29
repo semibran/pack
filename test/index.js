a76a5e96293414bbf742259ace52ce32ca6e3f3f
Brandon Semilla <semibran@gmail.com>
2018-01-28 23:58:18 -0800

    fix test

--- test/index.js
+++ test/index.js
@@ -1,20 +1,12 @@
 const pack = require("../")
 const render = require("./render")
-// const sizes = new Array(5)
-// for (let i = sizes.length; i--;) {
-//   sizes[i] = [
-//     Math.floor(Math.random() * 6) + 1,
-//     Math.floor(Math.random() * 6) + 1
-//   ]
-// }
-
-const sizes = [
-  [ 4, 2 ],
-  [ 4, 2 ],
-  [ 2, 4 ],
-  [ 2, 4 ],
-  [ 2, 2 ],
-]
+const sizes = new Array(5)
+for (let i = sizes.length; i--;) {
+  sizes[i] = [
+    Math.floor(Math.random() * 6) + 1,
+    Math.floor(Math.random() * 6) + 1
+  ]
+}
 
 let layout = pack(sizes)
 let canvas = render(layout)
