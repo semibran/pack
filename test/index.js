d18ffad0c9b27065568a9c233932bbc2ed3960f6
Brandon Semilla <semibran@gmail.com>
2018-01-29 17:27:30 -0800

    tweak test

--- test/index.js
+++ test/index.js
@@ -3,14 +3,15 @@ const render = require("./render")
 const sizes = new Array(6)
 for (let i = sizes.length; i--;) {
   sizes[i] = [
-    Math.floor(Math.random() * 6) + 1,
-    Math.floor(Math.random() * 6) + 1
+    Math.floor(Math.random() * 5) + 1,
+    Math.floor(Math.random() * 5) + 1
   ]
 }
+
+console.log(sizes)
 
 let layout = pack(sizes)
 let canvas = render(layout)
 document.body.appendChild(canvas)
 
-console.log(sizes)
 console.log(layout)
--- test/render.js
+++ test/render.js
@@ -1,23 +1,28 @@
-const scale = 16
-const colors = [ "maroon", "navy", "green", "olive", "purple", "teal" ]
+var colors = [ "maroon", "green", "olive", "navy", "purple", "teal" ]
+var scale = 16
 
 module.exports = function render(layout, canvas) {
   if (!canvas) canvas = document.createElement("canvas")
-  let context = canvas.getContext("2d")
-  canvas.width = Math.round(layout.size[0] * scale)
-  canvas.height = Math.round(layout.size[1] * scale)
-  for (let i = 0; i < layout.rects.length; i++) {
-    let rect = layout.rects[i]
-    let color = colors[i % colors.length]
+  var context = canvas.getContext("2d")
+  canvas.width = layout.size[0] * 16
+  canvas.height = layout.size[1] * 16
+  for (var i = 0; i < layout.rects.length; i++) {
+    var rect = layout.rects[i]
+    var color = colors[i]
     context.beginPath()
     context.rect(
-      Math.round(rect.position[0] * scale + 1),
-      Math.round(rect.position[1] * scale + 1),
-      Math.round(rect.size[0] * scale - 2),
-      Math.round(rect.size[1] * scale - 2)
+      rect.position[0] * scale + 2,
+      rect.position[1] * scale + 2,
+      rect.size[0] * scale - 4,
+      rect.size[1] * scale - 4
     )
     context.fillStyle = color
+    context.globalAlpha = 0.25
     context.fill()
+    context.globalAlpha = 1
+    context.strokeStyle = color
+    context.lineWidth = 2
+    context.stroke()
   }
   return canvas
 }
