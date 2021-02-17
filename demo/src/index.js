f2531a980226319975866ef42a778ab41de0eba2
Brandon Semilla <semibran@gmail.com>
2021-02-16 18:02:00 -0800

    stylistic updates

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,55 +1,65 @@
-import pack from "../../lib/pack"
+import pack from '../../lib/pack'
 
-const scale = 16
-const colors = [ "maroon", "navy", "green", "olive", "purple", "teal" ]
-const layout = pack(
-	new Array(16)
-	.fill()
-	.map(_ => [
-		Math.floor(Math.random() * 6) + 2,
-		Math.floor(Math.random() * 6) + 2
-	])
-)
+const TILE_SIZE = 16
+const MIN_RECT_SIZE = 2
+const MAX_RECT_SIZE = 8
+const colors = ['maroon', 'navy', 'green', 'olive', 'purple', 'teal']
 
-layout.boxes.sort((a, b) => a.position[0] - b.position[0])
-let canvas = render(layout)
+const getSize = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min
+
+const genRect = (min, max) => ({
+  width: getSize(min, max),
+  height: getSize(min, max)
+})
+
+const sizes = new Array(16).fill()
+  .map(() => genRect(MIN_RECT_SIZE, MAX_RECT_SIZE))
+
+const layout = pack(sizes)
+layout.rects.sort((a, b) => a.x - b.x)
+
+console.log(layout)
+
+const canvas = render(layout)
 document.body.appendChild(canvas)
 
 function render(layout, canvas) {
-	if (!canvas) canvas = document.createElement("canvas")
-	let context = canvas.getContext("2d")
-	canvas.width = layout.size[0] * scale
-	canvas.height = layout.size[1] * scale
-	context.fillStyle = "white"
-	context.fillRect(0, 0, canvas.width, canvas.height)
+  if (!canvas) canvas = document.createElement('canvas')
+  const context = canvas.getContext('2d')
+  canvas.width = layout.width * TILE_SIZE
+  canvas.height = layout.height * TILE_SIZE
+  context.fillStyle = 'white'
+  context.fillRect(0, 0, canvas.width, canvas.height)
 
-	for (let y = 0; y < layout.size[1]; y++) {
-		for (let x = 0; x < layout.size[0]; x++) {
-			context.beginPath()
-			context.rect(x * scale - 0.5, y * scale - 0.5, scale, scale)
-			context.strokeStyle = "silver"
-			context.stroke()
-		}
-	}
+  for (let y = 0; y < layout.height; y++) {
+    for (let x = 0; x < layout.width; x++) {
+      context.beginPath()
+      context.rect(x * TILE_SIZE - 0.5, y * TILE_SIZE - 0.5, TILE_SIZE, TILE_SIZE)
+      context.strokeStyle = 'silver'
+      context.stroke()
+    }
+  }
 
-	for (let i = 0; i < layout.boxes.length; i++) {
-		let box = layout.boxes[i]
-		let color = colors[i % colors.length]
-		let [ w, h ] = box.size
-		let [ x, y ] = box.position
+  for (let i = 0; i < layout.rects.length; i++) {
+    const { x, y, width, height } = layout.rects[i]
+    const color = colors[i % colors.length]
 
-		context.beginPath()
-		context.rect(x * scale + 1, y * scale + 1, w * scale - 3, h * scale - 3)
+    context.beginPath()
+    context.rect(x * TILE_SIZE + 1,
+                 y * TILE_SIZE + 1,
+                 width * TILE_SIZE - 3,
+                 height * TILE_SIZE - 3)
 
-		context.globalAlpha = 0.5
-		context.fillStyle = color
-		context.fill()
+    context.globalAlpha = 0.5
+    context.fillStyle = color
+    context.fill()
 
-		context.globalAlpha = 1
-		context.lineWidth = 2
-		context.strokeStyle = color
-		context.stroke()
-	}
+    context.globalAlpha = 1
+    context.lineWidth = 2
+    context.strokeStyle = color
+    context.stroke()
+  }
 
-	return canvas
+  return canvas
 }
